c0255c06542f1f8d539f32575726ccaf15091e26
Carol Cheung <carol.cheung@telus.com>
2023-01-09 15:13:54 -0800

    add state to component DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,17 +2,32 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
+  //destructure props
   const {days, day, setDay} = props;
 
+  //x different keys in the props obj
   return (
-  <ul>{days.map(day => (
+    <ul>{days.map(x => (
     <DayListItem
-    name={day.name}
-    spots={day.spots}
-    selected={day.name === props.day}
+      key={x.id}
+      name={x.name}
+      spots={x.spots}
+      selected={x.name === day}
     setDay={setDay}
     />
   ))}
   </ul>
 );
 };
+
+//originally like this on ln8
+// return (
+//   <ul>{days.map(day => (
+//     <DayListItem
+//     name={day.name}
+//     spots={day.spots}
+//     selected={day.name === props.day}
+//     setDay={props.setDay}
+//     />
+//   ))}
+//   </ul>
