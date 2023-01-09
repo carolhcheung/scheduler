28bee9bceea86b6f342690cfc0a9d9b59254fae3
Carol Cheung <carol.cheung@telus.com>
2023-01-09 14:44:54 -0800

    add dayList component and stories

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,35 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   })
 
+  const formatSpots = (spots) => {
+    if (spots > 1)
+      return `${spots} spots remaining`;
+    if (spots === 1)
+      return `${spots} spot remaining`;
+    if (spots < 1)
+      return "no spots remaining";
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
+
+// export default function DayListItem(props) {
+//   const dayClass = classNames('day-list__item', {
+//     "day-list__item--selected": props.selected,
+//     "day-list__item--full": props.spots === 0
+//   })
+
+//   return (
+//     <li className={dayClass} onClick={() => props.setDay(props.name)}>
+//       <h2 className="text--regular">{props.name}</h2>
+//       {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+//       {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+//       {props.spots < 1 && <h3 className="text--light">no spots remaining</h3>}
+//     </li>
+//   );
+// }
