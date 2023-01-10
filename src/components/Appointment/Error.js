539a6a40db7a2be7c45f8da773cd92bb26b106ae
Carol Cheung <carol.cheung@telus.com>
2023-01-09 21:31:48 -0800

    add notes to components

--- src/components/Appointment/Error.js
+++ src/components/Appointment/Error.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+//informs the user when an error occurs
 export default function Error(props) {
   return (
 <main className="appointment__card appointment__card--error">
