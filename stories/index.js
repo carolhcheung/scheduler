738ea91589ae6777974da8d4d2de8b32c75e7e4c
Carol Cheung <carol.cheung@telus.com>
2023-01-09 21:27:44 -0800

    add Error component and story

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -159,3 +160,4 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => <Error message="Could not delete appointment" />)
