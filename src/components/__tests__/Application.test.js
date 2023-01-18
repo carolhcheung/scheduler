5356be47f19f330eea6ff899d871751c71930954
Carol Cheung <carol.cheung@telus.com>
2023-01-17 22:11:04 -0800

    add debugging and complete no spots remaining test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import {
+  debug,
   prettyDOM,
   getAllByTestId,
   getByText,
@@ -40,15 +41,16 @@ describe("Application", () => {
 
   it.only("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     //Render the Application.
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
     // Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
     // console.log(prettyDOM(container));
     //gets all containers named data-testid=appointment
     const appointments = getAllByTestId(container, "appointment");
+    debug()
     // console.log(prettyDOM(appointments));
     //gets first container named data-testid=appointment[0]
-    const appointment = getAllByTestId(container, "appointment")[0];
+    const appointment = appointments[0];
     // console.log(prettyDOM(appointment));
     //Click the "Add" button on the first empty appointment.
     fireEvent.click(getByAltText(appointment, "Add"))
@@ -60,7 +62,6 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     //Click the "Save" button on that same appointment.
     fireEvent.click(getByText(appointment, "Save"));
-
     //Check that the element with the text "Saving" is displayed.
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
     //Wait until the element with the text "Lydia Miller-Jones" is displayed.
@@ -68,8 +69,8 @@ describe("Application", () => {
     //Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
-    );
-    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+      );
+      expect(getByText(day, "no spots remaining")).toBeInTheDocument();
     });
 })
 
